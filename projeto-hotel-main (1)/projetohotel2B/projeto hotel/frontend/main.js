af4b5158ffba2cd209d56dc1a971306e75fbbaf0
taliene246 <taliene.santos@escola.pr.gov.br>
2026-04-07 09:29:28 -0300

    Add files via upload

--- projeto-hotel-main (1)/projetohotel2B/projeto hotel/frontend/main.js
+++ projeto-hotel-main (1)/projetohotel2B/projeto hotel/frontend/main.js
@@ -6,17 +6,17 @@ document.addEventListener("DOMContentLoaded", function() {
   //aqui estamos pegando o formulario pelo id
   //no html presia exstir:<from id="cadastro">
   const formcadastro = document.getElementById("formcadastro");
-if (formcadastro) {
+     if (formcadastro) {
 
 
 
 
 
-    formcadastro.addEventListener("submit", async (e) =>{
+         formcadastro.addEventListener("submit", async (e) =>{
 
-        try{
+             try{
 
-            //envia os dados ao backend (rota/api/cadastrar)via POST
+                 //envia os dados ao backend (rota/api/cadastrar)via POST
                     const resp = await fetch('/api/cadastrar',{
                         method: 'POST',
                         headers: {'content-tipe': 'application/json'},
@@ -27,14 +27,12 @@ if (formcadastro) {
                     //exibe a mensagem de retorno para o usuario
                     this.document.getElementById ('mensagem').innerText = result.message;
                     formcadastro.requestFullscreen();
+            }
+                catch (err){
+                //caso algo de errado (servidor fora do ar, etc.)
+                alert('erro de comunicação com o servidor: ' + err);
 
-    
-        }
-        catch (err){
-            //caso algo de errado (servidor fora do ar, etc.)
-            alert('erro de comunicação com o servidor: ' + err);
-
-        }
+            }
         // agora vamos mostrar os dados no console (F12 → console)
 
 
@@ -54,10 +52,32 @@ if (formcadastro) {
         console.log("email:", dados.email);
         //mostra o campo de telefone (so funciona se existir no html)
         console.log("telefone:", dados.telefone);
-        //mostra o objeto completo com todos os dados
+        //mostra o objeto completo com todados
         console.log(dados);
     });
 }
 
-    if 
+    if (btnBuscar) {
+        btnBuscar.addEventListener ('click', async ()=> {
+
+            const nome= this.document.getElementById ('campo de busca'). value;
+            const resp= await fetch (`/buscar?nome=${nome}`);
+            const clientes= await resp.json() 
+            const tabela= document.getElementById ('tabeladeresultados');
+            tabela.innerHTML= '';
+
+            clientes.forEach(cli=> {
+                const row=
+                <tr>
+                    <td>${cli.ID}</td>
+                    <td>${cli.Nome}</td>
+                    <td>${cli.CPF}</td>
+                    <td>${cli.Email}</td>
+                    <td>${cli.Telefone}</td>
+                    <td><a href="/alterar?id=$(cil.ID)" class="bnt-warning">Editar</a>sm</td>
+                </tr>
+                tabela.innerHTML
+            });
+        });
+    }
 });
